ca68ed23e75bbca5861f3a5c2b1d7f5388daa8e3
vman241308 <vman241308@gmail.com>
2024-03-29 10:19:26 -0400

    feat: Completed verification code UI [pp-15]

--- src/pages/auth/verificationCode/index.jsx
+++ src/pages/auth/verificationCode/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
+import { CssBaseline, Box, Container, Typography } from "@mui/material";
+import PPCopyright from "@/components/PPCopyright";
 import { motion } from "framer-motion";
-
-import { Box } from "@mui/material";
 
 import { MuiOtpInput } from "mui-one-time-password-input";
 import backgroundImage from "@/assets/background.jpeg";
@@ -20,23 +20,30 @@ const VerificationCode = () => {
   };
 
   return (
-    <>
+    <div
+      className="w-full h-full bg-white text-black flex items-center justify-center"
+      style={{
+        backgroundImage: `url(${backgroundImage})`, // set image here
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
       <div
-        className="w-full h-full bg-white text-black flex items-center justify-center flex-col gap-5 font-sans"
-        style={{
-          backgroundImage: `url(${backgroundImage})`, // set image here
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
+        component="main"
+        className="w-full md:w-2/3 lg:w-1/2 xl:w-1/3 border border-solid border-gray-100 py-6 shadow-lg rounded-lg bg-white"
       >
+        <CssBaseline />
         <Box
           sx={{
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
           }}
+          className="flex gap-5"
         >
-          <p className="text-3xl">E-mail verification</p>
+          <Typography component="h1" variant="h5">
+            E-mail verification
+          </Typography>
           {!isSuccessed ? (
             <p className="text-xl">
               We've e-mailed your a 6 digit code. Please check your e-mail.
@@ -68,8 +75,9 @@ const VerificationCode = () => {
             </motion.div>
           )}
         </Box>
+        <PPCopyright sx={{ mt: 3 }} />
       </div>
-    </>
+    </div>
   );
 };
 
