18429f875a5bf41f6675c790ca6d0f866480242a
vman241308 <vman241308@gmail.com>
2024-06-12 12:02:55 -0700

    fix: console.error

--- src/Components/Options/VideoEditorInterface.jsx
+++ src/Components/Options/VideoEditorInterface.jsx
@@ -474,7 +474,7 @@ function VideoEditorInterface({
     } catch (error) {
       return;
     }
-    console.log(ffmpegCommand);
+    // console.log(ffmpegCommand);
     // console.log(aspectFFmpegCommand);
 
     try {
